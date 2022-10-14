23b27ce7d9c53f1fb66db1e99d822b03527e65bd
Ravi <selker.ravi@gmail.com>
2022-10-14 13:57:29 +0200

    Simplify clock example

--- examples/1-clock.py
+++ examples/1-clock.py
@@ -5,14 +5,9 @@ hours_el = Element("hours")
 minutes_el = Element("minutes")
 seconds_el = Element("seconds")
 
-
-async def update_clock():
-    while True:
-        now = datetime.now()
-        hours_el.write(now.strftime("%H"))
-        minutes_el.write(now.strftime("%M"))
-        seconds_el.write(now.strftime("%S"))
-        await asyncio.sleep(1)
-
-
-await loop.run_until_complete(update_clock())
+while True:
+    now = datetime.now()
+    hours_el.write(now.strftime("%H"))
+    minutes_el.write(now.strftime("%M"))
+    seconds_el.write(now.strftime("%S"))
+    await asyncio.sleep(1)
